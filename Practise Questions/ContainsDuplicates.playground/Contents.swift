b9def7c6aff05d2dd411a9550a1497e7b4c227d9
Navati Jain <>
2021-02-16 09:41:41 -0500

    Added a practise problem - Contains Duplicates

---
+++ Practise Questions/ContainsDuplicates.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+import XCTest
+
+var str = "Contains Duplicates"
+
+func containsDuplicates(_ arrayOfStrings: [String]) -> Bool {
+    var set: Set<String> = []
+    for currentString in arrayOfStrings {
+        if set.contains(currentString.lowercased()) {return true}
+        set.insert(currentString.lowercased())
+    }
+    return false
+}
+
+containsDuplicates(["has","having","will have"])
+containsDuplicates(["has","having","will have", "Has"])
+containsDuplicates(["has"])
+containsDuplicates([])
